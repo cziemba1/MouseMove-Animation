09b3d6e63d0f5ca1367e9ac5621a84adab165a23
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-01-29 22:41:28 -0300

    MouseMove Animation

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("working")
+$("#background-wrapper").mousemove(function (e) {
+
+    var moveX = (($(window).width() / 2) - event.pageX) * 0.02;
+    var moveY = (($(window).height() / 2) - event.pageY) * 0.02;
+    $(".bg-fruits").css("margin-left", moveX + "px");
+    $(".bg-fruits").css("margin-top", moveY + "px");
+})

4fc69f00fcaddc3d790ad7f8c4ff89a4b15724c6
Clau Ziemba <53158588+cziemba1@users.noreply.github.com>
2020-01-30 23:17:03 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-const letter = $(".letter");
-const navItem = $("#nav-items");
-const intro = $("#intro");
-const hidden = $(".hidden");
-const introContact = $("#intro-contact")
-const underline = $(".underline");
+$("#background-wrapper").mousemove(function (e) {
 
-let tl = gsap.timeline();
-
-tl
-    .from(navItem, 0.9, { x: 500, ease: "power1.easeOut" })
-    .staggerFrom(hidden, 1.2, { autoAlpha: 0, y: 30, ease: "power1.easeOut" }, .8, "-=1")
-    .from(introContact, 1.2, { autoAlpha: 0, y: 200, ease: "power1.easeOut" }, "-=1.5")
-    .staggerFromTo(letter, 1,
-        { autoAlpha: 0, x: 10 },
-        { autoAlpha: 1, x: -10, ease: Power1.easeOut }, 0.1, "-=1")
-    .to(underline, 1, { textDecoration: "underline", ease: "power1.easeIn" }, "-=0.5")
-
-
+    var moveX = (($(window).width() / 2) - event.pageX) * 0.02;
+    var moveY = (($(window).height() / 2) - event.pageY) * 0.02;
+    $(".bg-fruits").css("margin-left", moveX + "px");
+    $(".bg-fruits").css("margin-top", moveY + "px");
+})
